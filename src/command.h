1989ae19d698d901bc38c93d9194bb88bd79abd1
selva <selvakumar.jawahar@netrounds.com>
2022-12-30 23:31:36 +0530

    wip: Added async command class

--- src/command.h
+++ src/command.h
@@ -1,3 +1,49 @@
 #ifndef COMMAND_H
 #define COMMAND_H
+
+#include <future>
+
+namespace active_object
+{
+    template<typename DerivedT>
+    class AsyncCommandT
+    {
+        public:
+           auto GetFuture() const 
+            {
+                return Derived().GetFuture();
+            }
+            void Execute()
+            {
+                Derived().Execute();
+            }
+        protected:
+            ~AsyncCommandT() = default;
+        private:
+            DerivedT& Derived()
+            {
+                return static_cast<DerivedT&> (*this);
+            }
+            const DerivedT& Derived() const
+            {
+                return static_cast<const DerivedT&> (*this);
+            }
+    };
+
+    template<typename T>
+    class InferEmotions : AsyncCommandT<InferEmotions<T>>
+    {
+        std::future<T> GetFuture() const
+        {
+            return async_result.get_future();
+        }
+        void Execute()
+        {
+
+        }
+        private:
+            std::promise<T> async_result;
+    };
+}
+
 #endif
--- src/concurrent_queue.h
+++ src/concurrent_queue.h
@@ -1,3 +1,35 @@
 #ifndef CONCURRENT_QUEUE_H
 #define CONCURRENT_QUEUE_H
+
+#include <queue>
+#include <mutex>
+#include <optional>
+
+namespace active_object
+{
+    template<typename T>
+    class ConcurrentQueue
+    {
+        public:
+            void enqueue(T data) noexcept
+            {
+                std::lock_guard<std::mutex> guard(mtx);
+                queue.push(data);
+            }
+            std::optional<T> dequeue() noexcept
+            {
+                std::lock_guard<std::mutex> guard(mtx);
+                if (queue.empty())
+                {
+                    return std::nullopt;
+                }
+                auto data = queue.front;
+                queue.pop();
+                return data;
+            }
+        private:
+            std::queue<T> queue;
+            std::mutex mtx;
+    };
+}
 #endif 
