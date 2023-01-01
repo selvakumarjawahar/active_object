9c0429be1f13606f0ca02ef589b4a664d839332a
selva <selvakumar.jawahar@netrounds.com>
2023-01-01 22:54:12 +0530

    wip: Added executor

--- src/executor.h
+++ src/executor.h
@@ -1,3 +1,36 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
+
+#include "concurrent_queue.h"
+#include <chrono>
+#include <optional>
+#include <thread>
+
+namespace active_object
+{
+template<typename T>
+class Executor
+{
+    public:
+        explicit Executor(ConcurrentQueue<T>& cq):
+            queue_ref{cq}
+        {}
+        void Run()
+        {
+            while(true)
+            {
+                std::optional<T> command = queue_ref.dequeue();
+                if (!command)
+                {
+                    using namespace std::chrono_literals;
+                    std::this_thread::sleep_for(5ms);
+                    continue;
+                }
+                command.value()->Execute();
+            }
+        }
+    private:
+        ConcurrentQueue<T>& queue_ref;
+};
+}
 #endif
